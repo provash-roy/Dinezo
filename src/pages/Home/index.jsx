c49cf40ccb635a8ec6a40aff164113dbde0aca20
Provash-roy <provashray94@gmail.com>
2026-01-02 00:08:16 +0600

    after fetching restaurant

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,9 +1,36 @@
-import React from "react";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
 
 const Home = () => {
+  const [restaurants, setRestaurants] = useState([]);
+
+  useEffect(() => {
+    const fetchRestaurants = async () => {
+      try {
+        const res = await axios.get("/api/Restaurant");
+        setRestaurants(res.data);
+        console.log(res.data);
+      } catch (error) {
+        console.error("Error fetching restaurants:", error);
+      }
+    };
+
+    fetchRestaurants();
+  }, []);
+
   return (
     <div>
-      <p className="">Hello</p>
+      <h3>Restaurants</h3>
+      {restaurants.map((restaurant) => {
+        const { restaurantName, type, address } = restaurant;
+        return (
+          <div key={restaurant.id} className="border p-4 m-4">
+            <h4 className="text-2xl font-bold">{restaurantName}</h4>
+            <h4 className="text-2xl font-bold">{type}</h4>
+            <h4 className="text-2xl font-bold">{address}</h4>
+          </div>
+        );
+      })}
     </div>
   );
 };
